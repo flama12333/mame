ef4e14d981e23ac71bf8467641b020e7c3bc4452
flama12333 <143599905+flama12333@users.noreply.github.com>
2025-09-15 12:45:55 -0500

    despaced marywu.cpp

--- src/mame/misc/marywu.cpp
+++ src/mame/misc/marywu.cpp
@@ -235,7 +235,6 @@ void marywu_state::mary1s(machine_config &config)
     maincpu.set_addrmap(AS_IO, &marywu_state::io_map);
 }
 
-
 ROM_START( marywu )
 	ROM_REGION( 0x8000, "maincpu", 0 )
 	ROM_LOAD( "marywu_sunkiss_chen.rom", 0x0000, 0x8000, CRC(11f67c7d) SHA1(9c1fd1a5cc6e2b0d675f0217aa8ff21c30609a0c) )
